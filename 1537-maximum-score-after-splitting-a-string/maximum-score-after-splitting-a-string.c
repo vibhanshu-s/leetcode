96ef191037bdd346d8a9b060f95bc8b6fe7e90ec
Vibhanshu Singh <2k24.cs1q.2413927@gmail.com>
2025-01-01 14:52:11 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (38.46%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.c
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.c
@@ -1,12 +1,10 @@
 int maxScore(char* s) {
     int l=0,r=0,max=0;
-    int n=strlen(s);
-    for(int i=0;i<n;i++)
+    for(int i=0;i<strlen(s);i++)
         if(s[i]=='1') r++;
     
-    for(int i=1;i<n;i++){
-        if(s[i-1]=='0') l++;
-        else r--;
+    for(int i=1;i<strlen(s);i++){
+        s[i-1]=='0'?l++:r--;
         max=max>(l+r)?max:(l+r);
     }
     return max;
