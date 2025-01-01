c0f7804b62855cd5d2a32d4376702b5010f5f627
Vibhanshu Singh <2k24.cs1q.2413927@gmail.com>
2025-01-01 17:50:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (86.65%) - LeetSync

---
+++ 50-powx-n/powx-n.c
@@ -0,0 +1,12 @@
+
+double myPow(double x, int n) {
+    if(n<0) x=1/x;
+    double pow=1;
+    long num=labs(n);
+    while(num){
+        if(num&1) pow*=x;
+        x*=x;
+        num>>=1;
+    }
+    return pow;
+}
