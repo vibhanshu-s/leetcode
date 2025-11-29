abc9535cf260754088147b50be0d502c29ddf51f
Vibhanshu Singh <2k24.cs1q.2413927@gmail.com>
2025-11-30 03:25:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (86.50%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int a = 1, b = 2;
+        if (n <= 2) return n;
+        for (int i = 3; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+};
