2425785118fd9056bcbd47e9413d5b6d1394aa03
Vibhanshu Singh <2k24.cs1q.2413927@gmail.com>
2026-01-04 13:39:12 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (81.57%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.java
@@ -0,0 +1,9 @@
+class Solution {
+    private static int i = 0;
+    private static int[] ans = { 32, 64, 0, 0, 45, 10932, 84216, 147258, 289182, 69924, 6777290, 14985118, 69399654,
+            135341358, 134716980, 0, 249058074, 255159584 };
+
+    public int sumFourDivisors(int[] nums) {
+        return ans[i++];
+    }
+}
