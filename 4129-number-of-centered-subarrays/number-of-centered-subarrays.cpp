ce8e8d2642537faf8ed9ebf565c0635c243f0bea
Vibhanshu Singh <2k24.cs1q.2413927@gmail.com>
2026-01-12 11:33:03 +0530

    Time: 191 ms (76.92%) | Memory: 139.3 MB (61.54%) - LeetSync

--- 4129-number-of-centered-subarrays/number-of-centered-subarrays.cpp
+++ 4129-number-of-centered-subarrays/number-of-centered-subarrays.cpp
@@ -2,19 +2,25 @@ class Solution {
 public:
     int centeredSubarrays(vector<int>& nums) {
         int n = nums.size();
-        int ans = 0;
+        int ans = n; // all single-element subarrays
 
         for (int l = 0; l < n; l++) {
-            unordered_set<long long> st;
-            long long sum = 0;
+            unordered_set<int> st;
+            st.reserve(n);
+
+            int sum = 0;
+            int mn = INT_MAX, mx = INT_MIN;
 
             for (int r = l; r < n; r++) {
                 sum += nums[r];
+                mn = min(mn, nums[r]);
+                mx = max(mx, nums[r]);
                 st.insert(nums[r]);
 
-                if (st.count(sum)) {
-                    ans++;
-                }
+                if (r == l) continue; // skip length 1 (already counted)
+
+                if (sum < mn || sum > mx) continue;
+                if (st.count(sum)) ans++;
             }
         }
         return ans;
